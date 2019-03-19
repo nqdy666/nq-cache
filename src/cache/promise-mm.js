4acde1333c35700e9a8511f9f1566408d04d6124
nianqin <bu__dong@163.com>
2019-03-19 18:07:01 +0800

    添加注释

--- src/cache/promise-mm.js
+++ src/cache/promise-mm.js
@@ -2,6 +2,12 @@
 import store from './store'
 import { argToKey } from '../utils/assist'
 
+/**
+ * 异步的结果缓存在内存中，下次调用直接使用缓存数据
+ * @param {Function} fn - 纯函数
+ * @returns {Function} 和原函数的调用方式是一样的，第一次调用数据缓存后，之后的调用不会调用原函数，而是会直接使用缓存后的数据，另外该函数还带有一个静态的clearCache方法，可以用来清空缓存数据
+ * @property {Function} clearCache 清空缓存数据
+ */
 export default function promiseCache<F: Function> (fn: F): F {
   let cache = {}
   const cachedFn = (function cachedFn (...arg: Array<any>) {
