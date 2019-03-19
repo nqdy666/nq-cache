af2c674eba932299a7a64a7442ee45d1f6648187
nianqin <bu__dong@163.com>
2019-03-19 18:05:15 +0800

    update babel

--- src/cache/promise-ss.js
+++ src/cache/promise-ss.js
@@ -3,13 +3,6 @@ import store from './store'
 import { SessionStorage } from '../utils/storage'
 import { argToKey } from '../utils/assist'
 
-/**
- * 异步的结果缓存在session storage中，下次调用直接使用缓存数据
- * @param {Function} fn - 返回值是Promise的函数
- * @param {Function} storageKey - 缓存在session storage中的key前缀
- * @returns {Function} 和原函数的调用方式是一样的，第一次调用数据缓存后，之后的调用不会调用原函数，而是会直接使用缓存后的数据，另外该函数还带有一个静态的clearCache方法，可以用来清空缓存数据
- * @property {Function} clearCache 清空缓存数据
- */
 export default function promiseSessionStorageCache<F: Function> (fn: F, storageKey: string): F {
   let cache = SessionStorage.get(storageKey) || {}
   const cachedFn = (function cachedFn (...arg: Array<any>) {
--- src/cache/pure-func-mm.js
+++ src/cache/pure-func-mm.js
@@ -2,12 +2,6 @@
 import store from './store'
 import { argToKey } from '../utils/assist'
 
-/**
- * 纯函数执行的结果缓存在内存中，下次调用直接使用缓存数据
- * @param {Function} fn - 返回值是Promise的函数
- * @returns {Function} 和原函数的调用方式是一样的，第一次调用数据缓存后，之后的调用不会调用原函数，而是会直接使用缓存后的数据，另外该函数还带有一个静态的clearCache方法，可以用来清空缓存数据
- * @property {Function} clearCache 清空缓存数据
- */
 export default function pureFuncCache<F: Function> (fn: F): F {
   let cache = {}
   const cachedFn = (function cachedFn (...arg: Array<any>) {
