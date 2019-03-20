856a1046154b88c3f84679b4f0a8c5584ed429ec
nianqin <bu__dong@163.com>
2019-03-20 14:57:10 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export { argToKey } from './utils/assist'
-export promiseMemoryCache from './cache/promise-mm'
-export promiseSessionStorageCache from './cache/promise-ss'
-export pureFuncMemoryCache from './cache/pure-func-mm'
+export { default as promiseMemoryCache } from './cache/promise-mm'
+export { default as promiseSessionStorageCache } from './cache/promise-ss'
+export { default as pureFuncMemoryCache } from './cache/pure-func-mm'
 export { clearCache } from './cache/store'
