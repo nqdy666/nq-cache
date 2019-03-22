a8ca9480a5a620ec420217257ef20144beff60ff
nianqin <bu__dong@163.com>
2019-03-22 17:22:38 +0800

    add ts type

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,7 @@
-// todo
+declare module 'nq-cache' {
+  export function pureFuncMemoryCache(func: Function): any
+  export function promiseMemoryCache(func: Function): any
+  export function promiseSessionStorageCache(func: Function, key: string | void): any
+  export function clearCache(key: string | void): void
+  export function argToKey(...args:any[]): string
+}
