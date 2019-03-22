e28654d5ca71d85d0364dcff30adcd5e47d7160b
nianqin <bu__dong@163.com>
2019-03-22 18:08:09 +0800

    update example

--- examples/index.js
+++ examples/index.js
@@ -1,11 +1,19 @@
+
+
+function add(a, b) {
+  for (var i = 0; i < 10000000; i++) {}
+  return a + b
+}
+var addCache = cache.pureFuncMemoryCache(add)
+
 function testPureFuncMemoryCache() {
+  document.getElementById('pureFmRst01').innerText = ''
+  document.getElementById('pureFmTime01').innerText = ''
+  document.getElementById('pureFmRst02').innerText = ''
+  document.getElementById('pureFmTime02').innerText = ''
+
   var beginTime = 0
   var endTime = 0
-
-  function add(a, b) {
-    return a + b
-  }
-  var addCache = cache.pureFuncMemoryCache(add)
 
   beginTime = new Date().getTime()
   var addResult = addCache(1, 2) // run
@@ -20,18 +28,23 @@ function testPureFuncMemoryCache() {
   document.getElementById('pureFmTime02').innerText = endTime - beginTime
 }
 
+function request(data) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(data)
+    }, 2 * 1000)
+  })
+}
+var requestCache = cache.promiseMemoryCache(request)
+
 function testPromiseMemoryCache() {
+  document.getElementById('promiseFmRst01').innerText = ''
+  document.getElementById('promiseFmTime01').innerText = ''
+  document.getElementById('promiseFmRst02').innerText = ''
+  document.getElementById('promiseFmTime02').innerText = ''
+
   var beginTime = 0
   var endTime = 0
-
-  function request(data) {
-    return new Promise(function (resolve) {
-      setTimeout(function () {
-        resolve(data)
-      }, 2 * 1000)
-    })
-  }
-  var requestCache = cache.promiseMemoryCache(request)
 
   beginTime = new Date().getTime()
   requestCache('hello').then(function (res) {
@@ -49,18 +62,23 @@ function testPromiseMemoryCache() {
   })
 }
 
+function requestSS(data) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(data)
+    }, 2 * 1000)
+  })
+}
+var requestSSCache = cache.promiseSessionStorageCache(requestSS, 'requestSS')
+
 function testPromiseSessionStorageCache() {
+  document.getElementById('promiseFsRst01').innerText = ''
+  document.getElementById('promiseFsTime01').innerText = ''
+  document.getElementById('promiseFsRst02').innerText = ''
+  document.getElementById('promiseFsTime02').innerText = ''
+
   var beginTime = 0
   var endTime = 0
-
-  function request(data) {
-    return new Promise(function (resolve) {
-      setTimeout(function () {
-        resolve(data)
-      }, 2 * 1000)
-    })
-  }
-  var requestSSCache = cache.promiseSessionStorageCache(request, 'request')
 
   beginTime = new Date().getTime()
   requestSSCache('hello').then(function (res) {
@@ -76,6 +94,30 @@ function testPromiseSessionStorageCache() {
     document.getElementById('promiseFsRst02').innerText = res
     document.getElementById('promiseFsTime02').innerText = endTime - beginTime
   })
+}
+
+function clearPureFmFunction () {
+  addCache.clearCache()
+}
+
+function reRunPureFmFunction () {
+  testPureFuncMemoryCache()
+}
+
+function clearPromiseFmFunction () {
+  requestCache.clearCache()
+}
+
+function reRunPromiseFmFunction () {
+  testPromiseMemoryCache()
+}
+
+function clearPromiseFsFunction () {
+  requestSSCache.clearCache()
+}
+
+function reRunPromiseFsFunction () {
+  testPromiseSessionStorageCache()
 }
 
 testPureFuncMemoryCache()
